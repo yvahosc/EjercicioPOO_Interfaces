a6418e7995b9c152ae08af9a7bc0ace9748543f7
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-06-10 19:40:21 -0500

    Creación de código para la ejecución del desarrollo del ejercicio 2 dentro del método main del proyecto.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,15 @@
 package com.company;
 
 import com.company.Ejercicio1.Ejercicio1;
+import com.company.Ejercicio2.Ejercicio2;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Ejecución ejercicio 1.");
         Ejercicio1.ejecutarEjercicio();
+
+        System.out.println("Ejecución ejercicio 2.");
+        Ejercicio2.ejecutarEjercicio();
     }
 }
